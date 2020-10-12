9cc505992ae67b9737b93bbbadc2d4996a0b53a1
d-mcalpin <duncan.mcalpin@gmail.com>
2020-10-12 21:30:08 +0000

    Added places and markers to the Google Maps API

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -6,4 +6,34 @@
             lng: -8.1726,
         },
     });
+    $(".towns").click(function () {
+        
+        map.setCenter(new google.maps.LatLng(this.dataset.lat, this.dataset.lng));
+        map.setZoom(14);
+    });
+    const towns = [
+        ["midleton", 51.9143, -8.1726],
+        ["ballycotton", 51.8299, -8.0098],
+        ["castlemartyr", 51.9119, -8.0535],
+        ["cobh", 51.8503, -8.2943],
+        ["youghal", 51.9543, -7.8472],
+        
+    ];
+
+    let bounds = new google.maps.LatLngBounds();
+    for (i = 0; i < towns.length; i++) {
+        var marker = new google.maps.Marker({
+            position: {
+                lat: towns[i][1],
+                lng: towns[i][2],
+            },
+            title: towns[i][0],
+            map: map,
+        });
+        bounds.extend(marker.getPosition());
+    }
+    map.fitBounds(bounds);
 }
+
+
+
