018d39618840619d4ba3ccad5406a111f8dd71ca
d-mcalpin <duncan.mcalpin@gmail.com>
2020-10-13 09:34:51 +0000

    Added Footer and Adjusted Towns Section

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -9,7 +9,7 @@
     $(".towns").click(function () {
         
         map.setCenter(new google.maps.LatLng(this.dataset.lat, this.dataset.lng));
-        map.setZoom(14);
+        map.setZoom(12);
     });
     const towns = [
         ["midleton", 51.9143, -8.1726],
