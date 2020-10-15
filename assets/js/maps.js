fac0169817d980578fd2405a257e696affdc3850
d-mcalpin <duncan.mcalpin@gmail.com>
2020-10-15 15:47:00 +0000

    Added close function to infowindows

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -6,8 +6,7 @@ function loadMap() {
     center: { lat: 51.9143, lng: -8.1726 },
   };
 
-
-// Code as instructed by Google Documentation along with following the YouTube Tutorial by Traversy Media. Link in README
+  // Code as instructed by Google Documentation along with following the YouTube Tutorial by Traversy Media. Link in README
 
   var map = new google.maps.Map(document.getElementById("map"), options);
 
@@ -15,62 +14,74 @@ function loadMap() {
     {
       latlong: { lat: 51.9004, lng: -8.2923 }, // Fota Island Resort
       iconImage: "assets/images/hotel.png",
-      content: "<img src='assets/images/fotaresortsm.jpg'><h4>Fota Island Resort</h4><a href='index.html#windmills-info'>Click here to learn more about Fota Island Resort</a>",
+      content:
+        "<img src='assets/images/fotaresortsm.jpg'><h4>Fota Island Resort</h4><a href='index.html#windmills-info'>Click here to learn more about Fota Island Resort</a>",
     },
     {
       latlong: { lat: 51.9102, lng: -8.0538 }, //Castlemartyr Resort
       iconImage: "assets/images/hotel.png",
-      content: "<img src='assets/images/castlemartyrsm.jpg'><h4>Castlemartyr Resort</h4><a href='index.html#armenistis-info'>Click here to learn more about Catlemartyr Resort</a>",
+      content:
+        "<img src='assets/images/castlemartyrsm.jpg'><h4>Castlemartyr Resort</h4><a href='index.html#armenistis-info'>Click here to learn more about Catlemartyr Resort</a>",
     },
     {
-      latlong: { lat:  51.8651, lng: -8.0750 }, //Ballymaloe House
+      latlong: { lat: 51.8651, lng: -8.075 }, //Ballymaloe House
       iconImage: "assets/images/hotel.png",
-      content: "<img src='assets/images/little_venice_150px.jpg'><h4>Ballymaloe House</h4><a href='index.html#little-venice-info'>Click here to learn more about Ballymaloe House</a>",
+      content:
+        "<img src='assets/images/little_venice_150px.jpg'><h4>Ballymaloe House</h4><a href='index.html#little-venice-info'>Click here to learn more about Ballymaloe House</a>",
     },
     {
-      latlong: { lat: 51.8720, lng: -8.0160 }, //Garryvoe Hotel
+      latlong: { lat: 51.872, lng: -8.016 }, //Garryvoe Hotel
       iconImage: "assets/images/hotel.png",
-      content: "<img src='assets/images/garryvoesm.jpg'><h4>Garryvoe Hotel</h4><a href='index.html#panagia-info'>Click here to learn more about Garryvoe Hotel</a>",
+      content:
+        "<img src='assets/images/garryvoesm.jpg'><h4>Garryvoe Hotel</h4><a href='index.html#panagia-info'>Click here to learn more about Garryvoe Hotel</a>",
     },
     {
       latlong: { lat: 51.8345, lng: -8.2862 }, // Spike Island
       iconImage: "assets/images/historical.png",
-      content: "<img src='assets/images/spikeislandsm.jpg'><h4>Spike Island</h4><a href='index.html#hippie-fish-info'>Click here to learn more about Spike Island</a>",
+      content:
+        "<img src='assets/images/spikeislandsm.jpg'><h4>Spike Island</h4><a href='index.html#hippie-fish-info'>Click here to learn more about Spike Island</a>",
     },
     {
       latlong: { lat: 51.8515, lng: -8.2937 }, //Cobh Cathedral
       iconImage: "assets/images/cathedral.png",
-      content: "<img src='assets/images/cobhcathedralsm.jpg'><h4>Cobh Cathedral</h4><a href='index.html#kounelas-info'>Click here to learn more about Cobh Cathedral</a>",
+      content:
+        "<img src='assets/images/cobhcathedralsm.jpg'><h4>Cobh Cathedral</h4><a href='index.html#kounelas-info'>Click here to learn more about Cobh Cathedral</a>",
     },
     {
-      latlong: { lat: 51.8910, lng: -8.3070 }, //Fota Wildlife Park
+      latlong: { lat: 51.891, lng: -8.307 }, //Fota Wildlife Park
       iconImage: "assets/images/zoo.png",
-      content: "<img src='assets/images/fotawildlifesm.jpg'><h4>Fota Wildlife Park</h4><a href='index.html#lefteris-info'>Click here to learn more about Fota Wildlife Park</a>",
+      content:
+        "<img src='assets/images/fotawildlifesm.jpg'><h4>Fota Wildlife Park</h4><a href='index.html#lefteris-info'>Click here to learn more about Fota Wildlife Park</a>",
     },
     {
       latlong: { lat: 51.9132, lng: -8.1693 }, //Jameson Distillery
       iconImage: "assets/images/whiskey.png",
-      content: "<img src='assets/images/jamesonsm.jpg'><h4>Jameson Distillery</h4><a href='index.html#bakalo-info'>Click here to learn more about The Jameson Distillery</a>",
+      content:
+        "<img src='assets/images/jamesonsm.jpg'><h4>Jameson Distillery</h4><a href='index.html#bakalo-info'>Click here to learn more about The Jameson Distillery</a>",
     },
     {
       latlong: { lat: 51.8986795, lng: -8.30035294 }, // Fota Golf Club
       iconImage: "assets/images/golfing.png",
-      content: "<img src='assets/images/fotagolfclubsm.jpg'><h4>Fota Golf Club</h4><a href='index.html#mykonos-town-info'>Click here to learn more about Fota Golf Club</a>",
+      content:
+        "<img src='assets/images/fotagolfclubsm.jpg'><h4>Fota Golf Club</h4><a href='index.html#mykonos-town-info'>Click here to learn more about Fota Golf Club</a>",
     },
     {
       latlong: { lat: 51.90948055, lng: -8.06539906 }, //Castlemartyr Golf Club
       iconImage: "assets/images/golfing.png",
-      content: "<img src='assets/images/castlegolfsm.jpg'><h4>Castlemartyr Golf Club</h4><a href='index.html#ornos-info'>Click here to learn more about Castlemartyr Golf Club</a>",
+      content:
+        "<img src='assets/images/castlegolfsm.jpg'><h4>Castlemartyr Golf Club</h4><a href='index.html#ornos-info'>Click here to learn more about Castlemartyr Golf Club</a>",
     },
     {
       latlong: { lat: 51.8295455, lng: -8.0142264 }, //Ballycotton Cliff Walk
       iconImage: "assets/images/walkingtour.png",
-      content: "<img src='assets/images/ballycottoncliffsm.jpg'><h4>Ballycotton Cliff Walk</h4><a href='index.html#platis-info'>Click here to learn more about Ballycotton Cliff Walk</a>",
+      content:
+        "<img src='assets/images/ballycottoncliffsm.jpg'><h4>Ballycotton Cliff Walk</h4><a href='index.html#platis-info'>Click here to learn more about Ballycotton Cliff Walk</a>",
     },
     {
       latlong: { lat: 51.8583306964, lng: -8.00319395652 }, //Garryvoe Strand
       iconImage: "assets/images/walkingtour.png",
-      content: "<img src='assets/images/garryvoebeachsm.jpg'><h4>Garryvoe Beach</h4><a href='index.html#platis-info'>Click here to learn more about Garryvoe Beach</a>",
+      content:
+        "<img src='assets/images/garryvoebeachsm.jpg'><h4>Garryvoe Beach</h4><a href='index.html#platis-info'>Click here to learn more about Garryvoe Beach</a>",
     },
   ];
 
@@ -103,11 +114,15 @@ function loadMap() {
         infoWindow.open(map, marker);
         infowindow.close();
       });
-    }
 
+      map.addListener("click", function () {
+        if (infoWindow) infoWindow.close();
+      });
+            
+    }
   }
 
-// Code as instructed by Google Documentation along with YouTube Tutorial by Develop Mindfully. Link in ReadMe
+  // Code as instructed by Google Documentation along with YouTube Tutorial by Develop Mindfully. Link in ReadMe
 
   // Search Box
   var input = document.getElementById("search");
@@ -127,7 +142,6 @@ function loadMap() {
     markersPlaces.forEach(function (m) {
       m.setMap(null);
     });
-    
 
     var bounds = new google.maps.LatLngBounds();
     places.forEach(function (p) {
