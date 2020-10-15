0e36701cbf4e90ce46d29bb91180636cc1550744
d-mcalpin <duncan.mcalpin@gmail.com>
2020-10-15 16:59:11 +0000

    Amended search code to allow users to search outside of recommended markers

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -118,47 +118,68 @@ function loadMap() {
       map.addListener("click", function () {
         if (infoWindow) infoWindow.close();
       });
-            
+
     }
   }
 
   // Code as instructed by Google Documentation along with YouTube Tutorial by Develop Mindfully. Link in ReadMe
 
   // Search Box
-  var input = document.getElementById("search");
-  var searchBox = new google.maps.places.SearchBox(input);
-
-  map.addListener("bounds_changed", function () {
-    searchBox.setBounds(map.getBounds());
+  const input = document.getElementById("search");
+  const autocomplete = new google.maps.places.Autocomplete(input);
+  // Bind the map's bounds (viewport) property to the autocomplete object,
+  // so that the autocomplete requests use the current map bounds for the
+  // bounds option in the request.
+  autocomplete.bindTo("bounds", map);
+  // Set the data fields to return when the user selects a place.
+  autocomplete.setFields(["address_components", "geometry", "icon", "name"]);
+  const infowindow = new google.maps.InfoWindow();
+  const infowindowContent = document.getElementById("infowindow-content");
+  infowindow.setContent(infowindowContent);
+  const marker = new google.maps.Marker({
+    map,
+    anchorPoint: new google.maps.Point(0, -29),
   });
+  autocomplete.addListener("place_changed", () => {
+    infowindow.close();
+    marker.setVisible(false);
+    const place = autocomplete.getPlace();
 
-  var markersPlaces = [];
+    if (!place.geometry) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+      window.alert("No details available for input: '" + place.name + "'");
+      return;
+    }
 
-  searchBox.addListener("places_changed", function () {
-    var places = searchBox.getPlaces();
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17); // Why 17? Because it looks good.
+    }
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+    let address = "";
 
-    if (places.length == 0) return;
-
-    markersPlaces.forEach(function (m) {
-      m.setMap(null);
-    });
-
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function (p) {
-      if (!p.geometry) return;
-
-      markersPlaces.push(
-        new google.maps.Marker({
-          map: map,
-          title: p.name,
-          position: p.geometry.location,
-        })
-      );
-
-      if (p.geometry.viewport) bounds.union(p.geometry.viewport);
-      else bounds.extend(p.geometry.location);
-    });
-
-    map.fitBounds(bounds);
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] &&
+          place.address_components[0].short_name) ||
+          "",
+        (place.address_components[1] &&
+          place.address_components[1].short_name) ||
+          "",
+        (place.address_components[2] &&
+          place.address_components[2].short_name) ||
+          "",
+      ].join(" ");
+    }
+    infowindowContent.children["place-icon"].src = place.icon;
+    infowindowContent.children["place-name"].textContent = place.name;
+    infowindowContent.children["place-address"].textContent = address;
+    infowindow.open(map, marker);
   });
+ 
 }
