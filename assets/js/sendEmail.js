ffc19c794aca63a9e5159816ad2f7e9d69b09359
d-mcalpin <duncan.mcalpin@gmail.com>
2020-10-17 10:32:39 +0000

    Adjusted sendEmail JS to suit contact form

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,7 +1,16 @@
 function sendMail(contactForm) {
-    emailjs.send("default_service ", "template_xm6e2w4", {
+    emailjs.send("default_service", "template_xm6e2w4", {
         "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
+        "reply_to": contactForm.emailaddress.value,
         "information_request": contactForm.infosummary.value
     })
+    .then (
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    )
+    return false;
 }
